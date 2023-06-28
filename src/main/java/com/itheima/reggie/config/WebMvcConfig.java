63244205838ff88bbc204ad9ac06d8b3d8632958
twinkleding <twinkle415@163.com>
2023-06-28 21:11:52 +0800

    添加前端页面

---
+++ src/main/java/com/itheima/reggie/config/WebMvcConfig.java
@@ -0,0 +1,26 @@
+package com.itheima.reggie.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * @author TwinkleDing
+ */
+@Slf4j
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+
+    /**
+     * 设置静态资源映射
+     *
+     * @param registry 映射
+     */
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("开始静态资源映射");
+        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+}
